afed1216c887cd33dbeb38db84b89eb18dd28045
dudendy <a.dude.koval@gmail.com>
2022-03-18 16:39:00 +0700

    feat: test CD

--- src/root/Footer/Footer.js
+++ src/root/Footer/Footer.js
@@ -20,7 +20,7 @@ export default function Footer() {
                 <div>
                     <span className={classes.highlight}>v0.31</span>
                     <span className={classes.footerCopyright}>
-                        fireball.gg is the <a href='https://github.com/orden-gg/fireball' rel='noreferrer' target='_blank'>open-source</a>, <a href='https://www.aavegotchi.com/' rel='noreferrer' target='_blank'>gotchiverse</a> client focused on game {'&&'} market transparency. Developed by <a href='https://twitter.com/orden_gg' rel='noreferrer' target='_blank'>ordenGG</a> {'&'} contributors. 
+                        fireball.gg is the <a href='https://github.com/orden-gg/fireball' rel='noreferrer' target='_blank'>open-source</a>, <a href='https://www.aavegotchi.com/' rel='noreferrer' target='_blank'>gotchiverse</a> client focused on game {'&'} market transparency. Developed by <a href='https://twitter.com/orden_gg' rel='noreferrer' target='_blank'>ordenGG</a> {'&'} contributors. 
                     </span>
                 </div>
                 <div className={classes.buttons}>
